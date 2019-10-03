f7b40c8785189e345a66a80217592624c735d8d6
ScrumptiousDev <ryan.mingyu.choi@gmail.com>
2019-10-02 21:26:01 -0700

    Better way to get provider single item

--- lib/providers/products.dart
+++ lib/providers/products.dart
@@ -38,6 +38,10 @@ class Products with ChangeNotifier {
     return [..._items];
   }
 
+  Product findById(String id) {
+    return _items.firstWhere((prod) => prod.id == id);
+  }
+
   void addProduct() {
     // _items.add(value);
     notifyListeners();

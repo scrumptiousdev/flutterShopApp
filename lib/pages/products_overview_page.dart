2f2bcc143427917c119d0f30b5e560dd3e4d702e
ScrumptiousDev <ryan.mingyu.choi@gmail.com>
2019-10-02 22:32:56 -0700

    Global favorite filter but this is wrong

--- lib/pages/products_overview_page.dart
+++ lib/pages/products_overview_page.dart
@@ -1,13 +1,41 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
+import '../providers/products.dart';
 import '../widgets/products_grid.dart';
+
+enum FilterOptions {
+  Favorites,
+  All
+}
 
 class ProductsOverviewPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    final products = Provider.of<Products>(context, listen: false);
+
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Shop')
+        title: const Text('Shop'),
+        actions: <Widget>[
+          PopupMenuButton(
+            icon: Icon(Icons.more_vert),
+            itemBuilder: (_) => [
+              PopupMenuItem(
+                child: Text('Only Favorites'),
+                value: FilterOptions.Favorites
+              ),
+              PopupMenuItem(
+                child: Text('Show All'),
+                value: FilterOptions.All
+              )
+            ],
+            onSelected: (FilterOptions selectedValue) {
+              if (selectedValue == FilterOptions.Favorites) products.showFavoritesOnly();
+              else products.showAll();
+            }
+          )
+        ]
       ),
       body: ProductsGrid()
     );

dd96d2b1ccda41c3e708fb2b75ea522fe24d4e9e
ScrumptiousDev <ryan.mingyu.choi@gmail.com>
2019-10-02 22:13:44 -0700

    Best way of using Consumer for only part of widget to rebuild

--- lib/widgets/product_item.dart
+++ lib/widgets/product_item.dart
@@ -7,36 +7,38 @@ import '../providers/product.dart';
 class ProductItem extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Consumer<Product>(
-      builder: (ctx, product, child) => ClipRRect(
-        borderRadius: BorderRadius.circular(10),
-        child: GridTile(
-          child: GestureDetector(
-            child: Image.network(
-              product.imageUrl,
-              fit: BoxFit.cover
-            ),
-            onTap: () => Navigator.of(context).pushNamed(
-              ProductDetailPage.routeName,
-              arguments: product.id
-            )
+    final product = Provider.of<Product>(context, listen: false);
+
+    return ClipRRect(
+      borderRadius: BorderRadius.circular(10),
+      child: GridTile(
+        child: GestureDetector(
+          child: Image.network(
+            product.imageUrl,
+            fit: BoxFit.cover
           ),
-          footer: GridTileBar(
-            leading: IconButton(
+          onTap: () => Navigator.of(context).pushNamed(
+            ProductDetailPage.routeName,
+            arguments: product.id
+          )
+        ),
+        footer: GridTileBar(
+          leading: Consumer<Product>(
+            builder: (ctx, product, child) => IconButton(
               icon: Icon(product.isFavorite ? Icons.favorite : Icons.favorite_border),
               color: Theme.of(context).accentColor,
               onPressed: () => product.toggleFavoriteStatus()
-            ),
-            backgroundColor: Colors.black87,
-            title: Text(
-              product.title,
-              textAlign: TextAlign.center
-            ),
-            trailing: IconButton(
-              icon: Icon(Icons.shopping_cart),
-              color: Theme.of(context).accentColor,
-              onPressed: () {}
             )
+          ),
+          backgroundColor: Colors.black87,
+          title: Text(
+            product.title,
+            textAlign: TextAlign.center
+          ),
+          trailing: IconButton(
+            icon: Icon(Icons.shopping_cart),
+            color: Theme.of(context).accentColor,
+            onPressed: () {}
           )
         )
       )

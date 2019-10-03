b481f3db4ff3b3367ab3c4a6afbaa45e4fd1ea16
ScrumptiousDev <ryan.mingyu.choi@gmail.com>
2019-10-02 22:54:35 -0700

    Cart provider and set up multiple provider on main widget

--- lib/main.dart
+++ lib/main.dart
@@ -4,14 +4,18 @@ import 'package:provider/provider.dart';
 import './pages/products_overview_page.dart';
 import './pages/product_detail_page.dart';
 import './providers/products.dart';
+import './providers/cart.dart';
 
 void main() => runApp(ShopApp());
 
 class ShopApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return ChangeNotifierProvider.value(
-      value: Products(),
+    return MultiProvider (
+      providers: [
+        ChangeNotifierProvider.value(value: Products()),
+        ChangeNotifierProvider.value(value: Cart())
+      ],
       child: MaterialApp(
         title: 'Shop App',
         theme: ThemeData(

fc5fc7549f5cf59b12aafed3eb5f924786f637e1
ScrumptiousDev <ryan.mingyu.choi@gmail.com>
2019-10-04 00:26:22 -0700

    Add new image to items

--- lib/pages/edit_product_page.dart
+++ lib/pages/edit_product_page.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 import '../providers/product.dart';
+import '../providers/products.dart';
 
 class EditProductPage extends StatefulWidget {
   static const routeName = '/edit-product';
@@ -49,6 +51,8 @@ class _EditProductPageState extends State<EditProductPage> {
       return;
     }
     _form.currentState.save();
+    Provider.of<Products>(context, listen: false).addProduct(_editedProduct);
+    Navigator.of(context).pop();
   }
 
   @override

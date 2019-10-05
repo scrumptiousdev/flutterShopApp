5d603906f92379343f2c0991f97e46c17e7435de
ScrumptiousDev <ryan.mingyu.choi@gmail.com>
2019-10-05 14:27:54 -0700

    Custom fade transition between page navigation

--- lib/widgets/app_drawer.dart
+++ lib/widgets/app_drawer.dart
@@ -4,6 +4,7 @@ import 'package:provider/provider.dart';
 import '../providers/auth.dart';
 import '../pages/orders_page.dart';
 import '../pages/user_products_page.dart';
+import '../helpers/custom_route.dart';
 
 class AppDrawer extends StatelessWidget {
   @override
@@ -24,7 +25,9 @@ class AppDrawer extends StatelessWidget {
           ListTile(
             leading: Icon(Icons.payment),
             title: Text('Orders'),
-            onTap: () => Navigator.of(context).pushReplacementNamed(OrdersPage.routeName)
+            onTap: () => Navigator.of(context).pushReplacement(CustomRoute(
+              builder: (ctx) => OrdersPage()
+            ))
           ),
           Divider(),
           ListTile(

99cfe2e01eb5b1233e83e05841c6afdb09225230
ScrumptiousDev <ryan.mingyu.choi@gmail.com>
2019-10-04 23:55:42 -0700

    Auto logout when token expires

--- lib/providers/auth.dart
+++ lib/providers/auth.dart
@@ -1,4 +1,5 @@
 import 'dart:convert';
+import 'dart:async';
 import 'package:flutter/foundation.dart';
 import 'package:http/http.dart' as http;
 
@@ -8,6 +9,7 @@ class Auth with ChangeNotifier {
   String _token;
   DateTime _expiryDate;
   String _userId;
+  Timer _authTimer;
 
   bool get isAuth {
     return token != null;
@@ -40,6 +42,7 @@ class Auth with ChangeNotifier {
           seconds: int.parse(responseData['expiresIn'])
         )
       );
+      _autoLogout();
       notifyListeners();
     } catch (err) {
       throw err;
@@ -58,6 +61,16 @@ class Auth with ChangeNotifier {
     _token = null;
     _userId = null;
     _expiryDate = null;
+    if (_authTimer != null) {
+      _authTimer.cancel();
+      _authTimer = null;
+    }
     notifyListeners();
+  }
+  
+  void _autoLogout() {
+    if (_authTimer != null) _authTimer.cancel();
+    final timeToExpiry = _expiryDate.difference(DateTime.now()).inSeconds;
+    _authTimer = Timer(Duration(seconds: timeToExpiry), logout);
   }
 }

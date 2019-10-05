75c0ff672e971be06befde52d54aa6185fe88d85
ScrumptiousDev <ryan.mingyu.choi@gmail.com>
2019-10-05 12:35:15 -0700

    More animated widgets

--- lib/pages/auth_page.dart
+++ lib/pages/auth_page.dart
@@ -91,30 +91,38 @@ class _AuthCardState extends State<AuthCard> with SingleTickerProviderStateMixin
   };
   var _isLoading = false;
   final _passwordController = TextEditingController();
-  // AnimationController _controller;
-  // Animation<Size> _heightAnimation;
+  AnimationController _controller;
+  Animation<Offset> _slideAnimation;
+  Animation<double> _opacityAnimation;
 
-  // @override
-  // void initState() {
-  //   super.initState();
-  //   _controller = AnimationController(
-  //     vsync: this,
-  //     duration: Duration(milliseconds: 300)
-  //   );
-  //   _heightAnimation = Tween<Size>(
-  //     begin: Size(double.infinity, 260),
-  //     end: Size(double.infinity, 320)
-  //   ).animate(CurvedAnimation(
-  //     parent: _controller,
-  //     curve: Curves.fastOutSlowIn
-  //   ));
-  // }
+  @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(
+      vsync: this,
+      duration: Duration(milliseconds: 300)
+    );
+    _slideAnimation = Tween<Offset>(
+      begin: Offset(0, -1.5),
+      end: Offset(0, 0)
+    ).animate(CurvedAnimation(
+      parent: _controller,
+      curve: Curves.fastOutSlowIn
+    ));
+    _opacityAnimation = Tween(
+      begin: 0.0,
+      end: 1.0
+    ).animate(CurvedAnimation(
+      parent: _controller,
+      curve: Curves.easeIn
+    ));
+  }
 
-  // @override
-  // void dispose() {
-  //   super.dispose();
-  //   _controller.dispose();
-  // }
+  @override
+  void dispose() {
+    super.dispose();
+    _controller.dispose();
+  }
 
   void _showErrorDialog(String message) {
     showDialog(
@@ -169,10 +177,10 @@ class _AuthCardState extends State<AuthCard> with SingleTickerProviderStateMixin
   void _switchAuthMode() {
     if (_authMode == AuthMode.Login) {
       setState(() => _authMode = AuthMode.Signup);
-      // _controller.forward();
+      _controller.forward();
     } else {
       setState(() => _authMode = AuthMode.Login);
-      // _controller.reverse();
+      _controller.reverse();
     }
   }
 
@@ -220,13 +228,27 @@ class _AuthCardState extends State<AuthCard> with SingleTickerProviderStateMixin
                     _authData['password'] = value;
                   }
                 ),
-                if (_authMode == AuthMode.Signup) TextFormField(
-                  enabled: _authMode == AuthMode.Signup,
-                  decoration: InputDecoration(labelText: 'Confirm Password'),
-                  obscureText: true,
-                  validator: _authMode == AuthMode.Signup ? (value) {
-                    if (value != _passwordController.text) return 'Passwords do not match!';
-                  } : null
+                AnimatedContainer(
+                  duration: Duration(milliseconds: 300),
+                  constraints: BoxConstraints(
+                    minHeight: _authMode == AuthMode.Signup ? 60 : 0,
+                    maxHeight: _authMode == AuthMode.Signup ? 120 : 0
+                  ),
+                  curve: Curves.easeIn,
+                  child: FadeTransition(
+                    opacity: _opacityAnimation,
+                    child: SlideTransition(
+                      position: _slideAnimation,
+                      child: TextFormField(
+                        enabled: _authMode == AuthMode.Signup,
+                        decoration: InputDecoration(labelText: 'Confirm Password'),
+                        obscureText: true,
+                        validator: _authMode == AuthMode.Signup ? (value) {
+                          if (value != _passwordController.text) return 'Passwords do not match!';
+                        } : null
+                      )
+                    )
+                  )
                 ),
                 SizedBox(height: 20),
                 _isLoading ? CircularProgressIndicator() : RaisedButton(

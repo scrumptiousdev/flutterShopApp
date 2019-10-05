766ac263c69842db9511624a80b53caae40eae9f
ScrumptiousDev <ryan.mingyu.choi@gmail.com>
2019-10-04 18:53:34 -0700

    Storing orders in DB

--- lib/providers/orders.dart
+++ lib/providers/orders.dart
@@ -1,4 +1,6 @@
+import 'dart:convert';
 import 'package:flutter/foundation.dart';
+import 'package:http/http.dart' as http;
 
 import './cart.dart';
 
@@ -23,12 +25,26 @@ class Orders with ChangeNotifier {
     return [..._orders];
   }
   
-  void addOrder(List<CartItem> cartProducts, double total) {
+  Future<void> addOrder(List<CartItem> cartProducts, double total) async {
+    final timestamp = DateTime.now();
+    final response = await http.post('https://flutter-shop-app-8017a.firebaseio.com/orders.json',
+      body: json.encode({
+        'amount': total,
+        'dateTime': timestamp.toIso8601String(),
+        'products': cartProducts.map((cp) => {
+          'id': cp.id,
+          'title': cp.title,
+          'quantity': cp.quantity,
+          'price': cp.price
+        }).toList()
+      })
+    );
+
     _orders.insert(0, OrderItem(
-      id: DateTime.now().toString(),
+      id: json.decode(response.body)['name'],
       amount: total,
       products: cartProducts,
-      dateTime: DateTime.now()
+      dateTime: timestamp
     ));
 
     notifyListeners();

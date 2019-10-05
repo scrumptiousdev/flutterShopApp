643eac752448d6b63c787192552a67afa78a30c0
ScrumptiousDev <ryan.mingyu.choi@gmail.com>
2019-10-04 20:56:39 -0700

    Using FutureBuilder widget to replace unnecessary Statefulwidget for fetching data

--- lib/pages/orders_page.dart
+++ lib/pages/orders_page.dart
@@ -5,39 +5,36 @@ import '../providers/orders.dart' show Orders;
 import '../widgets/order_item.dart';
 import '../widgets/app_drawer.dart';
 
-class OrdersPage extends StatefulWidget {
+class OrdersPage extends StatelessWidget {
   static const routeName = '/orders';
 
   @override
-  _OrdersPageState createState() => _OrdersPageState();
-}
-
-class _OrdersPageState extends State<OrdersPage> {
-  var _isLoading = false;
-
-  @override
-  void initState() {
-    setState(() => _isLoading = true);
-    Provider.of<Orders>(context, listen: false).fetchAndSetOrders().then((_) {
-      setState(() => _isLoading = false);
-    });
-    super.initState();
-  }
-
-  @override
   Widget build(BuildContext context) {
-    final orderData = Provider.of<Orders>(context);
-
     return Scaffold(
       drawer: AppDrawer(),
       appBar: AppBar(
         title: const Text('Your Orders')
       ),
-      body: _isLoading ? Center(
-        child: CircularProgressIndicator()
-      ) : ListView.builder(
-        itemCount: orderData.orders.length,
-        itemBuilder: (ctx, i) => OrderItem(orderData.orders[i])
+      body: FutureBuilder(
+        future: Provider.of<Orders>(context, listen: false).fetchAndSetOrders(),
+        builder: (ctx, dataSnapshot) {
+          if (dataSnapshot.connectionState == ConnectionState.waiting) {
+            return Center(
+              child: CircularProgressIndicator()
+            );
+          } else {
+            if (dataSnapshot.error == null) {
+              return Consumer<Orders>(builder: (ctx, orderData, child) => ListView.builder(
+                itemCount: orderData.orders.length,
+                itemBuilder: (ctx, i) => OrderItem(orderData.orders[i])
+              ));
+            } else {
+              return Center(
+                child: Text('An error occurred!')
+              );
+            }
+          }
+        }
       )
     );
   }
